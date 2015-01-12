d71482be7b8e3479af77f8225e3549d9b05dc30b
Sourabh Desai <sourabhdesai@Sourabhs-MacBook-Air.local>
2015-01-12 11:07:53 -0600

    cleaned up some request handler code

--- src/goteller/requesthandler.go
+++ src/goteller/requesthandler.go
@@ -50,42 +50,26 @@ func (teller *GoTeller) handleRequest(connIO *bufio.ReadWriter) {
 	}
 
 	path := req.URL.Path[1:] // drop the leading '/'
-	match, err := regexp.MatchString("get/[0-9]+/..*", path)
+	var fileIdx uint32
+	var filename string
+	n, err := fmt.Sscanf(path, "get/%d/%s", &fileIdx, &filename)
 	if err != nil {
 		if teller.debugFile != nil {
 			fmt.Fprintln(*teller.debugFile, err)
 		}
-	}
-	if !match {
-		// Respond with 404
-		err := respondNotFound(connIO)
-		if err != nil {
-			if teller.debugFile != nil {
-				fmt.Fprintln(*teller.debugFile, err)
-			}
+	} else if n != 2 {
+		if teller.debugFile != nil {
+			fmt.Fprintf(*teller.debugFile, "Scanned %d out of 2 values in path \"%s\"", n, path)
 		}
 	} else {
-		var fileIdx uint32
-		var filename string
-		n, err := fmt.Sscanf(path, "get/%d/%s", &fileIdx, &filename)
+		// Valid request
+		bodyBuffer := teller.requestFunc(fileIdx, filename)
+		httpHeader := "HTTP 200 OK\r\nServer: Gnutella\r\nContent-type: application/binary\r\nContent-length: " + strconv.Itoa(len(bodyBuffer)) + "\r\n\r\n"
+		response := append([]byte(httpHeader), bodyBuffer...)
+		err := sendBytes(connIO, response)
 		if err != nil {
 			if teller.debugFile != nil {
 				fmt.Fprintln(*teller.debugFile, err)
-			}
-		} else if n != 2 {
-			if teller.debugFile != nil {
-				fmt.Fprintf(*teller.debugFile, "Scanned %d out of 2 values in path \"%s\"", n, path)
-			}
-		} else {
-			// Valid request
-			bodyBuffer := teller.requestFunc(fileIdx, filename)
-			httpHeader := "HTTP 200 OK\r\nServer: Gnutella\r\nContent-type: application/binary\r\nContent-length: " + strconv.Itoa(len(bodyBuffer)) + "\r\n\r\n"
-			response := append([]byte(httpHeader), bodyBuffer...)
-			err := sendBytes(connIO, response)
-			if err != nil {
-				if teller.debugFile != nil {
-					fmt.Fprintln(*teller.debugFile, err)
-				}
 			}
 		}
 	}
